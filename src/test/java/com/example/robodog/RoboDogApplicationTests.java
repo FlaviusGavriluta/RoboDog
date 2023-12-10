86dfed6940b8d3ce8675cdb58078cea5ed70db35
flaviusgavriluta <flavius.gavriluta@gmail.com>
2023-12-11 00:17:36 +0200

    start the project

---
+++ src/test/java/com/example/robodog/RoboDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.robodog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboDogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
